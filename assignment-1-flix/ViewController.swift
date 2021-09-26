f77f5749ced22c1142f7e5002b1cd207619d4040
Aramis Tanelus <aramis.tanelus@nyu.edu>
2021-09-26 13:38:23 -0400

    Initial Commit

---
+++ assignment-1-flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment-1-flix
+//
+//  Created by Aramis on 9/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
